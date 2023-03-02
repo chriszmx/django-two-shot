4fea85a2e5fa58b740bde5c5db33d910d9311d07
Chris <c.r.zambito@gmail.com>
2023-03-02 11:49:35 -0500

    Feature 4 Complete

--- receipts/models.py
+++ receipts/models.py
@@ -6,13 +6,17 @@ from django.conf import settings
 
 class ExpenseCategory(models.Model):
     name = models.CharField(max_length=50)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='categories')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name='categories')
 
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
     number = models.CharField(max_length=20)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accounts')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name='accounts')
 
 
 class Receipt(models.Model):
@@ -20,6 +24,13 @@ class Receipt(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=3)
     tax = models.DecimalField(max_digits=10, decimal_places=3)
     date = models.DateTimeField()
-    purchaser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receipts')
-    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE, related_name='receipts', null=True)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receipts', null=True)
+    purchaser = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE,
+                                  related_name='receipts')
+    category = models.ForeignKey(ExpenseCategory,
+                                 on_delete=models.CASCADE,
+                                 related_name='receipts')
+    account = models.ForeignKey(Account,
+                                on_delete=models.CASCADE,
+                                related_name='receipts',
+                                null=True)
